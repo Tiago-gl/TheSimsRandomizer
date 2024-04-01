f50a0d4f95f9f4beea81915faa468cdf7bea7a85
Tiago-gl <126834494+Tiago-gl@users.noreply.github.com>
2024-04-01 12:13:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById('show');
+const minhaDiv = document.getElementById('conteiner');
+
+botao.addEventListener('click', function() {
+    minhaDiv.style.display = 'block';
+});
